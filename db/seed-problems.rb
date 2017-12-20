cff94627f58d91dc64be46806e8bbb119ad129eb
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2017-12-20 10:09:38 +0900

    commit to master on imac3

--- db/seed-problems.rb
+++ db/seed-problems.rb
@@ -4,7 +4,7 @@ require 'sequel'
 DS = Sequel.mysql2("r99", user: "user", password: "pass")[:problems]
 
 num = 0
-File.foreach("r99.md","\n\n") do |line|
+File.foreach("r99.md","\n\n", encoding: "utf-8") do |line|
   if line =~ /^1\./
     num += 1
     DS.insert(num: num, detail: line.sub(/1\./,""), update_at: Time.now)
--- db/seed-users.rb
+++ db/seed-users.rb
@@ -3,7 +3,7 @@ require 'sequel'
 
 DS = Sequel.mysql2("r99", user: "user", password: "pass")[:users]
 
-File.foreach("sid-uid-myid-jname.txt") do |line|
+File.foreach("sid-uid-myid-jname.txt",encoding: "utf-8") do |line|
   next if line=~/^\*$/
   sid, uid, myid, jname =  line.chomp.split
   DS.insert(myid: myid, sid: sid, jname: jname, password: "robocar", update_at: Time.now)
