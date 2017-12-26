5f3a9879f0b2fc7b30af11fd3e7c686af45beb36
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2017-12-26 12:35:43 +0900

    tag 0.6.4

--- src/r99.lisp
+++ src/r99.lisp
@@ -2,7 +2,7 @@
   (:use :cl :cl-dbi :cl-who :cl-ppcre :cl-fad :hunchentoot))
 (in-package :r99)
 
-(defvar *version* "0.6.3")
+(defvar *version* "0.6.4")
 
 (defun getenv (name &optional default)
   "Obtains the current value of the POSIX environment variable NAME."
