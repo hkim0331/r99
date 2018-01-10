c7a1309fb7fd85a2dcda2ea7a3e31b08a556ae9a
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2018-01-10 11:46:04 +0900

    release 0.7.7
    does not include 8000's and 8001's answer in others-answers.

--- r99.asd
+++ r99.asd
@@ -3,7 +3,7 @@
 |#
 
 (defsystem "r99"
-:version "0.7.6"
+:version "0.7.7"
   :author "Hiroshi Kimura"
   :license ""
   :depends-on ("cl-dbi"
--- src/r99.lisp
+++ src/r99.lisp
@@ -2,7 +2,7 @@
   (:use :cl :cl-dbi :cl-who :cl-ppcre :cl-fad :hunchentoot))
 (in-package :r99)
 
-(defvar *version* "0.7.6")
+(defvar *version* "0.7.7")
 
 (defun getenv (name &optional default)
   "Obtains the current value of the POSIX environment variable NAME."
