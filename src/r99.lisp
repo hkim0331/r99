771712b4b25d59c117ebab27bf44b29e0225b25e
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2018-01-12 13:41:49 +0900

    release 0.8.2
    his/her last answer.

--- src/r99.lisp
+++ src/r99.lisp
@@ -2,7 +2,7 @@
   (:use :cl :cl-dbi :cl-who :cl-ppcre :cl-fad :hunchentoot))
 (in-package :r99)
 
-(defvar *version* "0.8.1")
+(defvar *version* "0.8.2")
 
 (defun getenv (name &optional default)
   "Obtains the current value of the POSIX environment variable NAME."
