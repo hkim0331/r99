9d14f10800395bc17ad4d6702f060429627b06f6
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2018-10-08 23:47:06 +0900

    deploy check してみるか

--- src/r99.lisp
+++ src/r99.lisp
@@ -128,9 +128,9 @@
        (:link
         :rel "stylesheet"
         :href
-        "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
         :integrity
-        "sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        "sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
         :crossorigin "anonymous")
        (:title "R99")
        (:link :type "text/css" :rel "stylesheet" :href "/r99.css"))
@@ -144,7 +144,23 @@
              ,@body
              (:hr)
              (:span "programmed by hkimura, release "
-                    (str *version*) "."))))))
+                    (str *version*) "."))
+       (:script
+        :src "https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        :integrity
+        "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        :crossorigin "anonymous")
+       (:script
+        :src
+        "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        :integrity
+        "sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        :crossorigin "anonymous")
+       (:script
+        :src "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        :integrity
+        "sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        :crossorigin "anonymous")))))
 
 (defun stars-aux (n ret)
   (if (zerop n) ret
@@ -236,6 +252,8 @@
          (query "select num, detail from problems order by num")))
     (page
       (:p (:img :src "/a-gift-of-the-sea.jpg" :width "100%"))
+      (:h1 :class "warn" "UNDER CONSTRUCTION")
+      (:p "問題も変えます。考え中です。" )
       (:h2 "problems")
       (loop for row = (dbi:fetch results)
          while row
