d0285b40027afbe01b53548ad6ab6c08593455cd
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2018-12-13 11:35:44 +0900

    update comment

--- src/r99.lisp
+++ src/r99.lisp
@@ -545,17 +545,21 @@ values ('~a', '~a', '~a', now())"
     (page
       (:h2 "submit your answer to " (str num))
       (:p (str d))
-      (:ul
-       (:li :class "warn" "動作確認しないで出すとマイナス。でかいぞ。")
+      (:ul :class "warn"
+       (:li "自分の理解を深めようとしない点数稼ぎは教員の労力、"
+            "採点の無駄時間が増えるだけ。<br>"
+            "やめましょう。"
+            "理解が深まらないままじゃ期末テストでも挽回できないよ。")
        (:li "ビルドできない回答は受け取らない。")
        (:li "回答を受け取ってもそれが正解とは限らない。")
        (:li "submit できたら、他の受講生の回答と自分の回答をよく見比べること。"))
       (:form :method "post" :action "/submit"
              (:input :type "hidden" :name "num" :value num)
              (:textarea :name "answer" :cols 60 :rows 10
-                        :placeholder "動作確認、
+                        :placeholder "プログラムの動作を確認後、
  correct indentation して、送信するのがルール。
- ケータイで回答もらって平常点インチキしても期末テストで確実に負けるから。
+ ケータイで回答もらって平常点インチキしても
+ 中間テスト・期末テストで確実に負けるから。
  マジ勉した方がいい。")
              (:br)
              (:input :type "submit")))))
