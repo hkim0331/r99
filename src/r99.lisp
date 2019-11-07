4587dc236b002c9d75a2b7f7f16dd95da9562cf2
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2019-11-07 12:28:14 +0900

    bootstrap 4.3.1

--- src/r99.lisp
+++ src/r99.lisp
@@ -3,6 +3,7 @@
 (in-package :r99)
 
 (defvar *version* "1.5.2")
+
 (defvar *nakadouzono* 8998)
 (defvar *hkimura* 8999)
 
@@ -148,15 +149,13 @@
        (:meta
         :name "viewport"
         :content "width=device-width, initial-scale=1.0")
-       (:link
-        :rel "stylesheet"
-        :href
-        "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-        :integrity
-        "sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
-        :crossorigin "anonymous")
-       (:title "R99")
-       (:link :type "text/css" :rel "stylesheet" :href "/r99.css"))
+       :link 
+      (:rel "stylesheet"
+        :href "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        :integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        :crossorigin="anonymous")
+      (:title "R99")
+      (:link :type "text/css" :rel "stylesheet" :href "/r99.css")
       (:body
        (:div :class "navbar navbar-default navbar-fixed-top"
              (:div :class "container"
@@ -168,23 +167,19 @@
              (:hr)
              (:span "programmed by hkimura, release "
                     (str *version*) "."))
+
        (:script
         :src "https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        :integrity
-        "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        :integrity "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
         :crossorigin "anonymous")
        (:script
-        :src
-        "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-        :integrity
-        "sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        :src "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        :integrity "sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
         :crossorigin "anonymous")
        (:script
-        :src "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-        :integrity
-        "sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-        :crossorigin "anonymous")))))
-
+        :src "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        :integrity "sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        :crossorigin "anonymous"))))))
 
 (defun stars-aux (n ret)
   (if (zerop n) ret
