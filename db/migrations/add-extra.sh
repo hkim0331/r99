e5ccfad2a48ac9dfc7109439253b4a9b0aaa84a5
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2019-12-26 22:44:00 +0900

    extra

--- db/migrations/add-extra.sh
+++ db/migrations/add-extra.sh
@@ -2,6 +2,6 @@
 for i in 111 112 113 114 115 116 117 118 119 120; do
     echo $i
     psql -U user1 -W -h localhost r99 <<EOF
-insert into problems (num, detail) values ('$i','なんか考える。乞うご期待');
+insert into problems (num, detail) values ('$i','[extra] なんか考える。乞うご期待');
 EOF
 done
