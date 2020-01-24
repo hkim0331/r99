ed9b111d638726a4b45fd035305aedc7e9abc9c5
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2020-01-24 11:38:10 +0900

    全角?問題は根が深い

--- src/r99.lisp
+++ src/r99.lisp
@@ -2,7 +2,7 @@
   (:use :cl :cl-dbi :cl-who :cl-ppcre :cl-fad :hunchentoot))
 (in-package :r99)
 
-(defvar *version* "1.22.6")
+(defvar *version* "1.22.7")
 
 (defvar *nakadouzono* 8998)
 (defvar *hkimura* 8999)
