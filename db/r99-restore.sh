609a6c0b3ce2046c089579d0f8edfdf98c6b0cb9
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2020-10-05 16:00:51 +0900

    なぜ、app.melt で動かないか？

--- db/r99-restore.sh
+++ db/r99-restore.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+if [ -z ${R99_HOST} ]; then
+  echo need source ../env.sh
+  exit
+end
 psql -h localhost -U ${R99_USER} -W ${R99_DB} < $1
--- db/tables-create.sql
+++ db/tables-create.sql
@@ -1,32 +1,29 @@
 create table answers (
-       id serial primary key,
+       id         serial primary key,
        myid       integer not null,
        num        integer not null,
        answer     text,
-       create_at  timestamp,
-       update_at  timestamp);
+       timestamp  timestamp);
 
 create table old_answers (
-       id serial primary key,
+       id         serial primary key,
        myid       integer not null,
        num        integer not null,
        answer     text,
-       create_at  timestamp);
+       timestamp  timestamp);
 
 create table problems (
-       id serial primary key,
-       num integer not null unique,
-       detail text,    -- detail じゃないけど、2017 を継承して。
-       create_at timestamp,
-       update_at timestamp);
+       id         serial primary key,
+       num        integer not null unique,
+       detail     text,    -- detail じゃないけど、2017 を継承して。
+       timestamp  timestamp);
 
 create table users (
-       id serial primary key,
+       id         serial primary key,
        myid       integer not null unique,
-       sid        varchar(8) not null unique,
+       sid        varchar(8) unique,
        jname      varchar(30),
        password   varchar(30),
-       create_at  timestamp,
-       update_at  timestamp);
+       timestamp  timestamp);
 
 
