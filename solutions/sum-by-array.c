34e1831d3a10ea11ad910ff6e9fcca17a2f30e0b
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2020-11-24 13:47:05 +0900

    add sum-by-array.c

--- solutions/sum-by-array.c
+++ solutions/sum-by-array.c
@@ -49,37 +49,27 @@ void print(int n, int a[]) {
   printf("\n");
 }
 
-void check1(void) {
-  int x[4]= {1,2,3,4};
-  int y[4]= {2,3,4,5};
-  int z[5];
+void check(int n, int x[], int y[]) {
+  int z[n+1];
 
-  plus(4, x, y, z);
-  print(5, z); // 3579 がプリントされるはず。
+  plus(n, x, y, z);
+  print(n+1, z); // 357 がプリントされるはず。
 }
 
-void check2(void) {
-  int x[]  = {4,5,6};
-  int y[]  = {8,9,0};
-  int z[4] = {};
-
-  plus(3, x, y, z);
-  print(4, z); // 1346 がプリントされるはず。
-}
-
-void check3(){
-  int x[] = {6,2,3,4,5,6,7,8,9,0,1,2,3};
-  int y[] = {5,4,3,2,1,0,5,4,3,2,1,0,5};
-  int z[14];
-
-  plus(13, x, y, z);
-  print(14, z); // 11666673322228 がプリントされるはず。
-}
 
 int main(void) {
-  check1();
-  check2();
-  check3();
+  int a[]= {1,2,3};
+  int b[]= {2,3,4};
+  check(3, a, b);
+
+  int c[]  = {4,5,6};
+  int d[]  = {8,9,0};
+  check(3, c, d);//1346
+
+  int x[] = {6,2,3,4,5,6,7,8,9,0,1,2,3};
+  int y[] = {5,4,3,2,1,0,5,4,3,2,1,0,5};
+  check(13, x, y); // 11666673322228 がプリントされるはず。
+
   return 0;
 }
 
