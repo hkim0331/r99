42abf888f3ab0d429218c0ce9345d3af1d07af0b
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2020-12-07 10:54:01 +0900

    imac3: update db/r99-restore.sh

--- db/r99-restore.sh
+++ db/r99-restore.sh
@@ -4,6 +4,10 @@ if [ -z "${R99_HOST}" ]; then
   exit
 fi
 
+if [ -z "$1" ]; then
+    echo "usage $0 backups/yyyy-mm-dd.sql"
+fi
+
 # must drop tables first.
 psql -h localhost -U ${R99_USER} -W ${R99_DB} < tables-drop.sql
 
