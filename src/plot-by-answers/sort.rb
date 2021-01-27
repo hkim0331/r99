c07ae3c741f236fec5dd1f9a2edbe335a07571c7
Hiroshi Kimura <hkim@melt.kyutech.ac.jp>
2021-01-27 12:29:21 +0900

    gitignore *.svg

--- src/plot-by-answers/sort.rb
+++ src/plot-by-answers/sort.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 
 num = Hash.new(0)
-(0..111).each do |n|
+(0..112).each do |n|
   num[n]=0
 end
 STDIN.readlines.each do |line|
   answered = line.strip.to_i
   num[answered] += 1
 end
-(0..111).each do |n|
+(0..112).each do |n|
   puts "#{n} #{num[n]}"
 end
